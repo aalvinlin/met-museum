ab9fea33965cdfc0f1c87907d7b3d5aa6ad077ec
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-08-09 23:52:51 -0700

    limit number of links shown in SearchNavigation

--- src/components/SearchNavigation.js
+++ src/components/SearchNavigation.js
@@ -5,14 +5,41 @@ const SearchNavigation = ({totalPages, resultsPerPage, currentPage}) => {
     // create array to map over
     let pageLinks = [];
 
-    for (let i = 0; i < totalPages; i += 1)
-        { pageLinks[i] = i; }
+    const maxLinksToShow = 9;
+
+    // display all pages if there aren't that many
+    if (totalPages <= maxLinksToShow)
+        {
+            for (let i = 0; i < totalPages; i += 1)
+                { pageLinks[i] = i + 1; }
+        }
+    // show first few pages, starting from 1
+    else if (currentPage <= Math.ceil(maxLinksToShow / 2))
+        {
+            for (let i = 0; i < maxLinksToShow; i += 1)
+                { pageLinks[i] = i + 1; }
+        }
+    // show last few pages
+    else if (currentPage >= totalPages - maxLinksToShow)
+        {
+            for (let i = totalPages - maxLinksToShow + 1; i <= totalPages; i += 1)
+                { pageLinks[i] = i + 1; }
+        }
+    // show pages around current page number
+    else
+        {
+            const lowerBound = currentPage - Math.ceil(maxLinksToShow / 2);
+            const upperBound = currentPage + Math.floor(maxLinksToShow / 2);
+
+            for (let i = lowerBound; i < upperBound; i += 1)
+                { pageLinks[i] = i + 1; }
+        }
 
     return (
         <div class="searchNavigation">
             {pageLinks.map(page => {
 
-                return <span key={`pageLink_${page}`}>{page}</span>
+                return <span key={`pageLink_${page}`}> {page} </span>
 
             })}
         </div>
