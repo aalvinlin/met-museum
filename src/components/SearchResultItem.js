faf97c3a8e338d04a5f534844bcd6f1c3cdf1f9b
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-08-12 20:15:26 -0700

    retrieve and display item information

--- src/components/SearchResultItem.js
+++ src/components/SearchResultItem.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const SearchResultItem = ({itemID}) => {
+
+    const [itemDetails, setItemDetails] = useState({});
+    
+    useEffect(() => {
+
+        axios.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + itemID)
+            .then(results => setItemDetails(results.data))
+            .catch(error => console.error(error))
+    }, []);
+
     return (
-        <div className="searchResultItem">{itemID}</div>
+        <div className="searchResultItem">
+            <img src={itemDetails.primaryImageSmall} alt={itemDetails.title} />
+            <p className="itemTitle">{itemDetails.title}</p>
+            <p className="itemArtist">{itemDetails.artistDisplayName}</p>
+            <p className="itemMiscDetails">
+                {itemDetails.objectDate}<br />
+                {itemDetails.medium}<br />
+            </p>
+        </div>
     )
 }
 
